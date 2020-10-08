50f4325f6bdfec3e0cee4a86f04d6a4160ec26fe
Elric920 <1871206768@qq.com>
2020-10-08 21:45:29 +0800

    第一次提交

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float smooth = 2.0f;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    private void Update()
+    {
+        Vector3 pos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, pos, smooth);
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h!=0||v!=0)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, 10, Time.deltaTime);
+            transform.Translate(new Vector3(h * Time.deltaTime * moveSpeed, 0, v * Time.deltaTime * moveSpeed));
+        }
+        else
+        {
+            moveSpeed = 0.0f;
+        }
+    }
+}
